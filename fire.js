24a088c2139f1f7f9885e7ec0e6bc767a3feea4b
Deny Kurniawan <dekakrens@gmail.com>
2022-01-10 21:30:10 +0700

    edit sesikit

--- fire.js
+++ fire.js
@@ -1,13 +1,13 @@
-const firebase = require('firebase');
+const firebase = require("firebase");
 
 //change the firebase configuration below according to your firebase SDK
 const config = {
-    apiKey: "AIzaSyBhLVI-6dcTvm-qBLF_pg95M293MFucS_U",
-    authDomain: "posyandu-75c76.firebaseapp.com",
-    projectId: "posyandu-75c76",
-    storageBucket: "posyandu-75c76.appspot.com",
-    messagingSenderId: "566794835776",
-    appId: "1:566794835776:web:4ff93ee19ce3bf15e2cca5"
+  apiKey: "AIzaSyCGqq6oMA1GTbURV0DC-X72ZEhj0aaiRgU",
+  authDomain: "esp32-hall-sensor.firebaseapp.com",
+  projectId: "esp32-hall-sensor",
+  storageBucket: "esp32-hall-sensor.appspot.com",
+  messagingSenderId: "251861365362",
+  appId: "1:251861365362:web:06ace13b6907d62e28c8eb",
 };
 
 const fire = firebase.initializeApp(config);
--- index.js
+++ index.js
@@ -1,49 +1,53 @@
-const app = require('express')();
-const fire = require('./fire');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const app = require("express")();
+const fire = require("./fire");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.send(
-        '<h1>Tes Express & Firebase Cloud Firestore</h1><ul><li><p><b>GET /data/esp32</b></p></li><li><p><b>POST /data/esp32</b>  => {suhu, tinggi, berat}</p></li></ul>'
-    );
+app.get("/", (req, res) => {
+  res.send(
+    "<h1>Tes Express & Firebase Cloud Firestore</h1><ul><li><p><b>GET /data/esp32</b></p></li><li><p><b>POST /data/esp32</b>  => {suhu, tinggi, berat}</p></li></ul>"
+  );
 });
 
 //match databaseName with your database name
-app.get('/databaseName', (req, res) => {
-    const db = fire.firestore();
-    let wholeData = []
-    db.collection('databaseName').orderBy('date', 'desc').get().then(snapshot => {
-        snapshot.forEach(doc => {
-            wholeData.push(doc.data())
-        });
-        res.send(wholeData)
-    }).catch(error => {
-        console.log('Error! ', error);
+app.get("/cadence", (req, res) => {
+  const db = fire.firestore();
+  let wholeData = [];
+  db.collection("cadenceSensor")
+    .orderBy("date", "desc")
+    .get()
+    .then((snapshot) => {
+      snapshot.forEach((doc) => {
+        wholeData.push(doc.data());
+      });
+      res.send(wholeData);
+    })
+    .catch((error) => {
+      console.log("Error! ", error);
     });
 });
 
-app.post('/databaseName', (req, res) => {
-    const db = fire.firestore();
-    db.collection('databaseName').add({
-        //change this collections according to your need
-        temperature: req.body.suhu,
-        height: req.body.tinggi,
-        weight: req.body.berat,
-        date: new Date()
-    });
-    res.send({
-        date: new Date(),
-        status: 'POST Success!'
-    });
+app.post("/cadence", (req, res) => {
+  const db = fire.firestore();
+  db.collection("cadenceSensor").add({
+    //change this collections according to your need
+    temperature: req.body.suhu,
+    height: req.body.tinggi,
+    weight: req.body.berat,
+    date: new Date(),
+  });
+  res.send({
+    date: new Date(),
+    status: "POST Success!",
+  });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server has been started. Listening on port ${PORT}`)
+  console.log(`Server has been started. Listening on port ${PORT}`);
 });

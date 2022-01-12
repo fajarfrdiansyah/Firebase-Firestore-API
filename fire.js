e58249e0865b8b7cdd17a525f79216959efe0087
fajarfrdiansyah <fajarfirdiansyah21@gmail.com>
2022-01-12 13:44:43 +0700

    Another new firebase. New Region

--- fire.js
+++ fire.js
@@ -2,12 +2,13 @@ const firebase = require("firebase");
 
 //change the firebase configuration below according to your firebase SDK
 const config = {
-  apiKey: "AIzaSyACGMOYQPl4giDJNsqdVTXuutjzKzx-UqE",
-  authDomain: "cadence-sensor-esp32.firebaseapp.com",
-  projectId: "cadence-sensor-esp32",
-  storageBucket: "cadence-sensor-esp32.appspot.com",
-  messagingSenderId: "799444608495",
-  appId: "1:799444608495:web:cf94896cd28081accbc21a",
+  apiKey: "AIzaSyCTf2RbdpNYsW3UUPOcdHcKEK2YTZdafTY",
+  authDomain: "esp32-cadence-sensor.firebaseapp.com",
+  projectId: "esp32-cadence-sensor",
+  storageBucket: "esp32-cadence-sensor.appspot.com",
+  messagingSenderId: "635512191661",
+  appId: "1:635512191661:web:5720dcc453c221d4c12424",
+  measurementId: "G-P5X013ZMK2"
 };
 
 const fire = firebase.initializeApp(config);
